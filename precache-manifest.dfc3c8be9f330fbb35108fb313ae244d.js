30703d1346885a6e4bdb2bd9827c3dfa1f2d71cc
Hasan TEKGÜL <hasan.tekgul@tuik.gov.tr>
2020-04-02 21:21:10 +0300

    Updates

---
+++ precache-manifest.dfc3c8be9f330fbb35108fb313ae244d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45e3c213c3981534588ba0dfb69255ac",
+    "url": "/blueprint-basit-tablo/index.html"
+  },
+  {
+    "revision": "79d4c0b3b3f1720568ed",
+    "url": "/blueprint-basit-tablo/static/css/2.95ba6e23.chunk.css"
+  },
+  {
+    "revision": "fe924ad7299c9de00c2a",
+    "url": "/blueprint-basit-tablo/static/css/main.4c48667a.chunk.css"
+  },
+  {
+    "revision": "79d4c0b3b3f1720568ed",
+    "url": "/blueprint-basit-tablo/static/js/2.9ff23a32.chunk.js"
+  },
+  {
+    "revision": "b578178f33e8b4baed76c7b8f7ce2e07",
+    "url": "/blueprint-basit-tablo/static/js/2.9ff23a32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe924ad7299c9de00c2a",
+    "url": "/blueprint-basit-tablo/static/js/main.bbb06837.chunk.js"
+  },
+  {
+    "revision": "7f5211b2523e3b6627c7",
+    "url": "/blueprint-basit-tablo/static/js/runtime-main.9cb382fe.js"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/blueprint-basit-tablo/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/blueprint-basit-tablo/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/blueprint-basit-tablo/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/blueprint-basit-tablo/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/blueprint-basit-tablo/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/blueprint-basit-tablo/static/media/icons-20.cef8cdbb.woff"
+  }
+]);
